05dd2d4822f5b44db954510d85072627459a75a2
Vince W <vincewallgren@me.com>
2020-10-27 03:32:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,7 @@
 index = 0
 def position_taken?(board, index)
-  
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || "O"
+    return true
 end
